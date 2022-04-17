291ad5a610d666c4d406923a7d9e2c026c01952f
David Placca <dhegreatprosperity10@gmail.com>
2022-04-17 11:06:29 +0000

    refactor: improve functionality

--- script.py
+++ script.py
@@ -1,25 +1,30 @@
 from moviepy.editor import VideoFileClip
 from utils import extensions
+from pathlib import Path
+import subprocess
 import sys
 import os
+import re
 
 '''
 Audio Extractor 1.0.0
+Command line version
 
 The last argument is considered to be the destination folder, and therefore can
 accept several source files. ie. source1, source2, source..., destination.
-If only two arguments are passed, it is assumed that both arguments are source
-(contains source files) and destination folders respectively
+If more than one argument is passed, the last is assumed to be the destination folder.
+To override this behaviour, include --use-default to use the default destination as the destination folder
 
-Command line version
 '''
-# try:
-_, *sources = sys.argv
-# except ValueError:
-# 	sources = folder = None
 
-# if not sources and folder:
-# 	sources, folder = [folder], None
+_, *sources = sys.argv
+OVERRIDE_DST = False
+
+# identify destination folder
+if '--use-default' not in sources and len(sources) > 1:
+	OVERRIDE_DST = True
+	specified_destination = Path(sources.pop())
+
 
 def extract_and_save(file, save_at):
 	video_file = VideoFileClip(file)
@@ -27,18 +32,20 @@ def extract_and_save(file, save_at):
 	audio = video_file.audio
 	if audio:
 		basename = f'{os.path.basename(file).rsplit(".", 1)[0]}.mp3'
-		destination = os.path.join(save_at, basename)
+		destination = save_at.joinpath(basename)
 		overwrite = True
 
-		if os.path.exists(destination):
+		if destination.exists():
 			# get user prompt if the file exists
-			prompt = input(f'{basename!r} already exists in the destination '
-				'folder.\nWould you like to overwrite this file? (y/n): ')
+			prompt = input(
+				f'{basename!r} already exists in the destination '
+				'folder.\nWould you like to overwrite this file? (y/n): '
+			)
 			overwrite = prompt.lower() == 'y'
 
 		# overwrite existing file if user response is True
 		if overwrite:
-			audio.write_audiofile(destination)
+			audio.write_audiofile(destination.as_posix())
 		else:
 			print('Overwrite canceled!')
 
@@ -64,33 +71,40 @@ def parse_files(directory, level=1):
 					), level+1)
 	return output
 
-def create_destination(count):
+def get_destination(count):
 	import time
 	# create a destination folder
 	current_time = time.strftime('%d.%m.%y', time.localtime())
-	original = os.path.join(
+	destination = Path(os.path.join(
 		os.environ['USERPROFILE'],
 		'Documents',
 		'AudioExtractor'
-	)
+	))
 
-	if not os.path.exists(original):
-		os.mkdir(original)
+	if OVERRIDE_DST:
+		destination = specified_destination
 
 	if count > 1:
-		import random
-		return os.path.join(
-			original,
-			f'Extracts{current_time}.{random.randrange(101, 500)}'
-		)
-		del random
+		compiled = input(
+			f"\nYou're about to save multiple files to {destination.as_posix()!r}"
+			"\nWould you like to proceed (p) or create (c) a new folder?: "
+		).lower()
+
+		if compiled == 'c':
+			dn = input('Please enter the folder name. You can press enter to skip (We will create one for you): ')
+			import random
+			destination = destination.joinpath(
+				dn or \
+				f'Extracts{current_time}.{random.randrange(101, 500)}'
+			)
+			del random
 
 	del time
 
-	return original
+	return destination
 
 def is_video_file(fp):
-	ext = os.path.basename(fp).split('.')[-1]
+	_, ext = os.path.splitext(os.path.basename(fp))
 	return ext.lower() in extensions['video']
 
 
@@ -102,17 +116,14 @@ if sources:
 
 		for i, file in enumerate(sources, start=1):
 			print(f'{i}. {os.path.basename(file)!r}')
-		# print(f'Saving to {folder!r}')
 
-		proceed = input('Would you like to proceed? (y/n): ').lower()
+		number_of_files = len(sources)
+		folder = get_destination(number_of_files)
+		print(f'\nExtracting to {folder.as_posix()!r}')
+
+		proceed = input('\nWould you like to proceed? (y/n): ').lower()
 		if proceed == 'y':
-			number_of_files = len(sources)
-			folder = create_destination(number_of_files)
-
-			# create directory if it doesn't exist
-			if not os.path.exists(folder):
-				os.mkdir(folder)
-
+			folder.mkdir(exist_ok=True, parents=True)
 			# start extraction
 			extracted = 0
 			for n, src in enumerate(sources, start=1):
@@ -131,7 +142,9 @@ if sources:
 			print(f'Extraction complete! {extracted} '
 				f'file{["s", ""][extracted == 1]} extracted.')
 
-			os.startfile(folder)
+			# os.startfile(folder)
+			if extracted:
+				subprocess.run(f'explorer.exe /select, {folder.resolve()}')
 
 			# clear garbage
 			del sources, folder
